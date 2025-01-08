5ae475384d4b5a1f600ecdc88c82e37a76641186
skoved <sam.koved@gmail.com>
2025-01-07 22:27:03 -0500

    remove empty line

--- ch1/temps.c
+++ ch1/temps.c
@@ -6,7 +6,6 @@
 
 /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
 int main() {
-
     for (float fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
         float celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
