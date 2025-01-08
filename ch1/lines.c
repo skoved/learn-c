0dca73f6a349071a240e526fee2a27dee66e3794
skoved <sam.koved@gmail.com>
2025-01-07 23:12:02 -0500

    count tabs and spaces from input too

--- ch1/lines.c
+++ ch1/lines.c
@@ -2,12 +2,18 @@
 
 /* count lines in input */
 int main() {
-    int c, nl;
+    int c, nl, tab, space;
     nl = 0;
+    tab = 0;
+    space = 0;
     while ((c = getchar()) != EOF) {
         if (c == '\n') {
             nl++;
+        } else if (c == ' ') {
+            space++;
+        } else if (c == '\t') {
+            tab++;
         }
     }
-    printf("%d\n", nl);
+    printf("newlines: %d tabs: %d spaces: %d\n", nl, tab, space);
 }
